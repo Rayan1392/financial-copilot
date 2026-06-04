3188fff3a82629ff1797cee952f8702231e63a3e
Kamran Abdi <kamranabdi@outlook.com>
2026-06-04 13:51:42 +0330

    migration

---
+++ src/backend/FinancialCopilot.Infrastructure/Billing/Persistence/Migrations/20260604093445_UpdateFreePlanIncludedCredits.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinancialCopilot.Infrastructure.Billing.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFreePlanIncludedCredits : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "billing_subscription_plans",
+                keyColumn: "Code",
+                keyValue: "Free",
+                column: "IncludedCredits",
+                value: 10000m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "billing_subscription_plans",
+                keyColumn: "Code",
+                keyValue: "Free",
+                column: "IncludedCredits",
+                value: 10m);
+        }
+    }
+}
